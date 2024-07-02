c2cc858a19422d9d46c2df2f3c15584c5e00eb79
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-07-02 16:44:32 +0530

    updater

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -17,10 +17,31 @@
 # PixelProject OTA update package
 
 CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CUSTOM_VERSION).zip
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+
+CL_CYN="\033[36m"
+CL_PRP="\033[35m"
+
 
 .PHONY: bacon
-bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
+bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(CUSTOM_TARGET_PACKAGE)" >&2
+	@echo -e ${CL_CYN}""${CL_CYN}
+	@echo -e ${CL_PRP}""${CL_CYN}
+	@echo -e ${CL_CYN}"  _____ _______   ________ _        _____  _____   ____       _ ______ _____ _______ "${CL_CYN}
+	@echo -e ${CL_CYN}" |  __ \_   _\ \ / /  ____| |      |  __ \|  __ \ / __ \     | |  ____/ ____|__   __|"${CL_CYN}
+	@echo -e ${CL_CYN}" | |__) || |  \ V /| |__  | |      | |__) | |__) | |  | |    | | |__ | |       | |   "${CL_CYN}
+	@echo -e ${CL_CYN}" |  ___/ | |   > < |  __| | |      |  ___/|  _  /| |  | |_   | |  __|| |       | |   "${CL_CYN}
+	@echo -e ${CL_CYN}" | |    _| |_ / . \| |____| |____  | |    | | \ \| |__| | |__| | |___| |____   | |   "${CL_CYN}
+	@echo -e ${CL_CYN}" |_|   |_____/_/ \_\______|______| |_|    |_|  \_\\____/ \____/|______\_____|  |_|   "${CL_CYN}
+	@echo -e ${CL_CYN}""${CL_CYN}
+	@echo -e ${CL_CYN}""${CL_CYN}
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	$(hide) ./vendor/aosp/tools/generate_json_build_info.sh $(CUSTOM_TARGET_PACKAGE)
+	echo -e ${CL_BLD}${CL_CYN}"Datetime :"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(CUSTOM_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(CUSTOM_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(CUSTOM_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(CUSTOM_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_CYN}
